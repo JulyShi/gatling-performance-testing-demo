6e7a7cd7cbef66fa48599ea20fd8d8d758ea44d2
JulyxyShi <julyfly321520@163.com>
2018-08-06 10:10:20 +0800

    first commit

---
+++ src/test/scala/ticket/Ticket.scala
@@ -0,0 +1,71 @@
+package ticket
+
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.http.Predef._
+
+class Ticket extends Simulation {
+
+  val httpConf = http.baseURL("http://127.0.0.1:8888")
+
+  object ticketCode {
+
+    val ticketCode = exec(
+      http( "get ticket code" )
+        .post( "/code/myd1" )
+        .header( "Content-Type", "application/x-www-form-urlencoded" )
+        .formParam("idnum", "61032119000203132X")
+        .formParam("idtype", "2")
+//        .body( StringBody(
+//          """{
+//            |"idnum":"61032119000203132X",
+//            |"idtype":"1",
+//            |}""".stripMargin)).asJSON
+        .check( status.is( 200 ) )
+        .check(jsonPath("$.account").find.saveAs("account"))
+        .check(jsonPath("$.password").find.saveAs("password")))
+  }
+
+  object login {
+    val random = new scala.util.Random
+
+    // Generate a random string of length n from the given alphabet
+    def randomString(alphabet: String)(n: Int): String =
+      Stream.continually(random.nextInt(alphabet.size)).map(alphabet).take(n).mkString
+
+    // Generate a random alphabnumeric string of length n
+    def randomAlphanumericString(n: Int) =
+      randomString("abcdef0123456789")(n)
+
+    def mac(): String = {
+      randomAlphanumericString(12).grouped(2).toList.mkString(":")
+    }
+
+    //val random = new util.Random
+    //val randomMac = Iterator.continually(Array.fill[String](6)(f"${random.nextInt(255)}%02x").mkString(":"))
+
+    val body = StringBody(
+      """{
+        |"username": "${account}",
+        |"password": "${password}",
+        |"mac": "${mac}"
+        |}""".stripMargin)
+
+    val login = exec(session => {
+      session.set("mac", mac())
+    }).pause(2).exec(
+      http( "login with ticket" )
+        .post( "/auth" )
+        .header( "Content-Type", "application/json" )
+        .body(body).asJSON
+        .check(status.is( 200 )))
+  }
+
+val scn = scenario("ticket").during(60){exec( ticketCode.ticketCode, login.login )}
+
+  // 立即启动16个用户，执行60s
+  setUp(
+    scn.inject(atOnceUsers(16))
+  ).protocols(httpConf)
+
+}
---
+++ src/test/scala/ticket/Ticket1.scala
@@ -0,0 +1,67 @@
+package ticket
+
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class Ticket1 extends Simulation {
+
+  val httpConf = http.baseURL("http://127.0.0.1:8888")
+
+  object ticketCode {
+
+    val ticketCode = exec(
+      http( "get ticket code" )
+        .post( "/code/myd1" )
+        .header( "Content-Type", "application/x-www-form-urlencoded" )
+        .formParam("idnum", "61032119000203132X")
+        .formParam("idtype", "2")
+        .check( status.is( 200 ) )
+        .check(jsonPath("$.account").find.saveAs("account"))
+        .check(jsonPath("$.password").find.saveAs("password")))
+  }
+
+  object login {
+    val random = new scala.util.Random
+
+    // Generate a random string of length n from the given alphabet
+    def randomString(alphabet: String)(n: Int): String =
+      Stream.continually(random.nextInt(alphabet.size)).map(alphabet).take(n).mkString
+
+    // Generate a random alphabnumeric string of length n
+    def randomAlphanumericString(n: Int) =
+      randomString("abcdef0123456789")(n)
+
+    def mac(): String = {
+      randomAlphanumericString(12).grouped(2).toList.mkString(":")
+    }
+
+    //val random = new util.Random
+    //val randomMac = Iterator.continually(Array.fill[String](6)(f"${random.nextInt(255)}%02x").mkString(":"))
+
+    val body = StringBody(
+      """{
+        |"username": "${account}",
+        |"password": "${password}",
+        |"mac": "${mac}"
+        |}""".stripMargin)
+
+    val login = exec(session => {
+      session.set("mac", mac())
+    }).pause(2).exec(
+      http( "login with ticket" )
+        .post( "/auth" )
+        .header( "Content-Type", "application/json" )
+        .body(body).asJSON
+        .check(status.is( 200 )))
+  }
+
+val scn = scenario("ticket").during(2 hours){exec( ticketCode.ticketCode, login.login )}
+
+  //600秒启动100个用户，持续跑2小时
+  setUp(
+      scn.inject(rampUsers(100) over (60 seconds) )
+  ).protocols(httpConf)
+
+}
